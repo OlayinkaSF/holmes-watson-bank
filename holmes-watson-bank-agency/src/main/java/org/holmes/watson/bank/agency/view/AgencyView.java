889c79b3b58d9a382ec638e1e8dd74bf90c6d4dd
Olayinka SF <olayinka.sf@gmail.com>
2015-01-28 14:44:31 +0000

    init 1.3

--- holmes-watson-bank-agency/src/main/java/org/holmes/watson/bank/agency/view/AgencyView.java
+++ holmes-watson-bank-agency/src/main/java/org/holmes/watson/bank/agency/view/AgencyView.java
@@ -46,7 +46,7 @@ public class AgencyView extends javax.swing.JFrame implements TransactionListene
         jButton4 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(960, 540));
         setMinimumSize(new java.awt.Dimension(960, 540));
         setResizable(false);
